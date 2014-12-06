baaf3188cf05efa599dd689d7ae8fec6f97498a9
Eric Cloninger <ericc@pobox.com>
2014-12-05 23:16:20 -0600

    Fixed license files.  Basic README.md.  Cleanup.
    Removed several unused XML files.  Put in a placeholder widget file in assets.

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
-cgeo-gear
+#cgeo-gear
 =========
 
-Geocaching on Samsung Gear watch with data pushed from c:geo
+c:geo-gear is an Android app that is a companion to the popular 
+(c:geo Geocaching)[https://play.google.com/store/apps/details?id=cgeo.geocaching] app.
+
+Data from c:geo is transmitted to c:geo-gear and on to a Tizen-based Samsung Gear S 
+watch, allowing the user to locate geocaches with a Gear watch, leaving their phone
+in their pocket.
+
+The behavior of the app is simple. If the user launches it, there is only a description
+of the app and a button to launch c:geo.  If c:geo doesn't exist, the button is not 
+enabled.  
+
+The user makes use of c:geo-gear by launching c:geo and opening a geocache from it's list.  
+Inside the list is a button for launching external map and navigation apps.  One of these 
+items is named "Android Wear". When the user chooses this item, an intent is launched and
+c:geo-gear will respond to it by filling out it's UI with a few data points about the 
+geocache. The button then allows the user to send a Rich Notification to the watch that
+contains the cache name, cache code, and coordinates.  Once the notification is sent
+c:geo-gear quits and control returns to c:geo.
+
+#Building c:geo-gear
+=======================
+
+Build the project with Eclipse and the Android ADT plugins.  There is not a gradle project
+for c:geo-gear.  Import into Eclipse as any Android project and build.  The dependent libraries
+are from the Samsung developer site.
+
+#TODO
+======
+
+* Finish the Tizen widget
+
+* Separate the main Activity into two. One for user launch and one responding to the intent.
+The current behavior is modal and requires some switching back and forth.
+
+* Create an about box with credits.
+
+* Work to get a separate intent for Gear with the c:geo team.  Allow the addition of hint 
+to the message.
+
+* Work on the callback so the user can press "Found it" on the Tizen app and be returned to
+c:geo for the log.
+
+* Clean up the notification content.
+
+#Credits
+=========
+
+* The (c:geo team)[https://github.com/cgeo] for fixing a bug that caused their app to crash when this app was loaded and borrowing some of their icons
+
+* Cullin Moran (@culmor30)[https://github.com/culmor30] for the c:geo-wear project, where I got a few pointers on making a 
+plugin for c:geo. In particular, the GearService class
+
+* The globe image on the notification comes [Font Awesome](https://github.com/FortAwesome/Font-Awesome).
--- src/com/purplefoto/cgeogear/CacheNotification.java
+++ src/com/purplefoto/cgeogear/CacheNotification.java
@@ -1,29 +1,33 @@
+/*
+	Copyright 2014 Eric Cloninger (purplefoto.com)
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
+// Adapted from sample code on developer.samsung.com/gear
+
 package com.purplefoto.cgeogear;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.net.Uri;
-import android.text.format.Time;
 
 import com.samsung.android.sdk.richnotification.SrnAction;
-import com.samsung.android.sdk.richnotification.SrnAction.CallbackIntent;
 import com.samsung.android.sdk.richnotification.SrnImageAsset;
 import com.samsung.android.sdk.richnotification.SrnRichNotification;
 import com.samsung.android.sdk.richnotification.SrnRichNotification.AlertType;
 import com.samsung.android.sdk.richnotification.actions.SrnHostAction;
-import com.samsung.android.sdk.richnotification.actions.SrnRemoteInputAction;
-import com.samsung.android.sdk.richnotification.actions.SrnRemoteInputAction.InputModeFactory;
-import com.samsung.android.sdk.richnotification.actions.SrnRemoteInputAction.KeyboardInputMode;
-import com.samsung.android.sdk.richnotification.actions.SrnRemoteInputAction.KeyboardInputMode.KeyboardType;
-import com.samsung.android.sdk.richnotification.templates.SrnPrimaryTemplate;
-import com.samsung.android.sdk.richnotification.templates.SrnSecondaryTemplate;
-import com.samsung.android.sdk.richnotification.templates.SrnStandardSecondaryTemplate;
 import com.samsung.android.sdk.richnotification.templates.SrnStandardTemplate;
 import com.samsung.android.sdk.richnotification.templates.SrnStandardTemplate.HeaderSizeType;
 
@@ -50,6 +54,7 @@ public class CacheNotification {
         smallHeaderTemplate.setBackgroundColor(Color.rgb(69, 114, 32));
 
         // Create the actions
+        // TODO: Figure out why Action button colors are close to background color
         ArrayList<SrnAction> actions = new ArrayList<SrnAction>();
         SrnHostAction primaryAction = new SrnHostAction(mContext.getString(R.string.navigate));
         Bitmap primaryActionIcon = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.globe);
