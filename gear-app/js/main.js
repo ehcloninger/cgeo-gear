1f816f41b073993deb8948aad2760fbcca08a2e7
Eric Cloninger <ericc@pobox.com>
2014-12-11 15:20:05 -0800

    Project cleanup. New images.
    Make app draw different image on click to test image rotation. Decided to not use
    ImageMagick for rotation.  Changed Photoshop source to use layers instead.
    Change .gitignore to have Tizen Wearable specific files.

--- gear-app/js/main.js
+++ gear-app/js/main.js
@@ -1,12 +1,19 @@
+window.onload = function() {
+	// TODO:: Do your initialization job
 
-window.onload = function () {
-    // TODO:: Do your initialization job
+	// add eventListener for tizenhwkey
+	document.addEventListener('tizenhwkey', function(e) {
+		if (e.keyName == "back")
+			tizen.application.getCurrentApplication().exit();
+	});
 
-    // add eventListener for tizenhwkey
-    document.addEventListener('tizenhwkey', function(e) {
-        if(e.keyName == "back")
-            tizen.application.getCurrentApplication().exit();
-    });
-
-     
+	var index = 1;
+	var files = ["000", "045", "090", "135", "180", "225", "270", "315"];
+	var compass = document.querySelector('.contents');
+	compass.addEventListener("click", function() {
+		img = document.querySelector('#compass');
+		img.src = "images/" + files[index++] + ".png";
+		if (index > 7)
+			index = 0;
+	});
 };
