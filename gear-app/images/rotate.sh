58aab5d36453d093f96bad8ddf2108edb00b2c6e
Eric Cloninger <ericc@pobox.com>
2014-12-10 16:22:52 -0800

    Update image files using ImageMagick rotation.

---
+++ gear-app/images/rotate.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash          
+
+convert 000.png -distort SRT 45  045.png
+convert 000.png -distort SRT 90  090.png
+convert 000.png -distort SRT 135 135.png
+convert 000.png -distort SRT 180 180.png
+convert 000.png -distort SRT 225 225.png
+convert 000.png -distort SRT 270 270.png
+convert 000.png -distort SRT 315 315.png
